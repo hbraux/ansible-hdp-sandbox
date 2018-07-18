ee61277667869263df95694a102bf326e65a1ad3
Harold Braux <harold.braux@email.com>
2018-07-18 14:31:37 +0200

    minor change

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 if [[ -n $1 ]]
-then opts="-i $1,"; shift
+then host=$(grep $1 /etc/hosts | head -1 |  awk '{print $2}')
+     opts="-i $host,"
+     echo "executing playbook on $host"
+     shift
 else opts="--connection=local -i $(uname -n),"
 fi
 
-[[ ${1:0:2} == -- ]] &&  opts="$opts --tags ${1:2}"
+if [[ ${1:0:2} == -- ]] 
+then opts="$opts --tags ${1:2}"
+fi
 
 [[ -n  $http_proxy ]] && opts="$opts -e http_proxy=$http_proxy -e https_proxy=$http_proxy -e no_proxy=$no_proxy"
 
